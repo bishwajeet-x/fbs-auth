bd1836b744fab4de39dd4903d476ad02ff9ec595
bishwajeet <bishwajeetmandal93@gmail.com>
2022-10-25 13:44:16 +0530

    changes

---
+++ src/main/java/com/flightapp/api/controller/ApiController.java
@@ -0,0 +1,30 @@
+package com.flightapp.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.flightapp.api.dto.AirlineDto;
+
+@RestController
+@RequestMapping("/v1/api/airlines")
+public class ApiController {
+	
+	@Value("${airlineurl}") String airlineUrl;
+	
+	private static RestTemplate template = new RestTemplate();
+
+	@GetMapping("/")
+	public List<AirlineDto> fetchAllAirlines() {
+		ResponseEntity<Object> airlines = template.exchange(airlineUrl, 
+				HttpMethod.GET, null, 
+				Object.class);
+		return (List<AirlineDto>) airlines.getBody();
+	}
+}
