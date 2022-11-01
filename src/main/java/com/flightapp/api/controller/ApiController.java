b8ef3126319fe8dc1a05b093b4cdc3c651c70ea6
bishwajeet <bishwajeetmandal93@gmail.com>
2022-11-01 16:27:48 +0530

    INIT

--- src/main/java/com/flightapp/api/controller/ApiController.java
+++ src/main/java/com/flightapp/api/controller/ApiController.java
@@ -1,35 +1,33 @@
 package com.flightapp.api.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.flightapp.api.dto.AirlineDto;
 
 @RestController
-@RequestMapping("/v1/api/airlines")
+@RequestMapping("/api/auth")
 public class ApiController {
 	
-	@Value("${airlineurl}") String airlineUrl;
+	//@Value("${airlineurl}") String airlineUrl;
 	
-	private static RestTemplate template = new RestTemplate();
+	//private static RestTemplate template = new RestTemplate();
 
+//	@GetMapping("/")
+//	public List<AirlineDto> fetchAllAirlines() {
+//		ResponseEntity<Object> airlines = template.exchange(airlineUrl, 
+//				HttpMethod.GET, null, 
+//				Object.class);
+//		return (List<AirlineDto>) airlines.getBody();
+//	}
+	
 	@GetMapping("/")
-	public List<AirlineDto> fetchAllAirlines() {
-		ResponseEntity<Object> airlines = template.exchange(airlineUrl, 
-				HttpMethod.GET, null, 
-				Object.class);
-		return (List<AirlineDto>) airlines.getBody();
+	private String some() {
+		return "hello";
 	}
 	
 	@GetMapping("/test")
-	private String some() {
+	private String someTest() {
+		System.out.println("Test");
 		return "hello";
 	}
 }
